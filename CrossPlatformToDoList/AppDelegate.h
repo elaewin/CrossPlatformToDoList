178b394a406bd9c974a30dc5571e806648a5b3a2
Erica Winberry <elaewin@gmail.com>
2017-05-08 10:14:08 -0700

    adding initial project.

---
+++ CrossPlatformToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrossPlatformToDoList
+//
+//  Created by Erica Winberry on 5/8/17.
+//  Copyright © 2017 Erica Winberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrossPlatformToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrossPlatformToDoList
+//
+//  Created by Erica Winberry on 5/8/17.
+//  Copyright © 2017 Erica Winberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
