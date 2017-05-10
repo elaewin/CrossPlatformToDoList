e5c3c58013fc647514b1cfe260842892fbcfeb48
Erica Winberry <elaewin@gmail.com>
2017-05-10 10:18:59 -0700

    update gitignore to include credentials file.

---
+++ TVTodoList/Credentials.h
@@ -0,0 +1,13 @@
+//
+//  Credentials.h
+//  CrossPlatformToDoList
+//
+//  Created by Erica Winberry on 5/10/17.
+//  Copyright © 2017 Erica Winberry. All rights reserved.
+//
+
+#ifndef Credentials_h
+#define Credentials_h
+
+
+#endif /* Credentials_h */
